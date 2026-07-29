8f0961afecec65bcba03d8790d99ed12f8bb12dd
gojuwon354-cmd <gojuwon354@gmail.com>
2026-07-29 17:38:39 +0900

    사용하지 않는 실험용 제어 경로 제거

--- ros2_ws/src/pinkk_usb_insertion/scripts/cartesian_smoke_test_client.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""소스 트리에서 Cartesian smoke-test 노드를 실행하는 얇은 wrapper."""
-
-from pinkk_usb_insertion.cartesian_smoke_test_node import main
-
-
-if __name__ == '__main__':
-    main()
